57da91965dc5cc93289edb70d150bdb72f0d583a
EscVel <suhithegde@gmail.com>
2025-03-19 14:51:53 +0530

    cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "RISHIT RASTOGI - PES2UG22CS446\n";
+
   return 0;
 }
